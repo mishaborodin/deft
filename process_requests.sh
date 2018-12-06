a0fe479ad8d90ce2c4f7bdacf0dbf07d9126e509
retmas-dv <retmas@gmail.com>
2018-12-06 14:31:28 +0300

    Added process_requests.sh run script

---
+++ process_requests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# PRODSYS_BASE_DIR=/data/prodsys
+# */10 * * * * /bin/nice -n 0 $PRODSYS_BASE_DIR/deftcore/process_requests.sh $PRODSYS_BASE_DIR MC > /tmp/$USER.process_requests.sh.log 2>&1
+# vim /etc/security/limits.conf
+# atlswing - priority -20
+
+base_path=$1
+log_path="/tmp/${USER}.process_requests.log"
+
+cd ${base_path}/deftcore
+/usr/local/bin/python2.7 manage.py runworker -n process_requests -t $2 > ${log_path} 2>&1

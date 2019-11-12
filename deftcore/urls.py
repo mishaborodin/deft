3a8aae6d34337fa7e0239208855c5a9e42222729
retmas-dv <retmas@gmail.com>
2019-11-12 17:59:29 +0300

    Updated settings, urls and messaging listener

--- deftcore/urls.py
+++ deftcore/urls.py
@@ -6,7 +6,7 @@ from api import views
 from api.resources import InstanceResource, RequestResource, TaskResource, TRequestResource, TStepResource
 from django.contrib import admin
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 admin.autodiscover()
 
@@ -18,8 +18,8 @@ default_api.register(TRequestResource())
 default_api.register(TStepResource())
 
 urlpatterns = [
-    url(r'^$', lambda r: HttpResponseRedirect(reverse('api_dispatch_list',
-                                                      kwargs={'resource_name': 'instance', 'api_name': 'v1'}))),
+    url(r'^$', lambda r: HttpResponseRedirect(
+        reverse('api_dispatch_list', kwargs={'resource_name': 'instance', 'api_name': 'v1'}))),
     url(r'^test/', views.test_view),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(default_api.urls)),

6a5591420c00a94d2ee245d6aad8bbecbaa203d5
mishaborodin <borodin.misha@gmail.com>
2021-04-29 16:32:51 +0200

    Add check for pile tasks

--- taskengine/taskdef.py
+++ taskengine/taskdef.py
@@ -3802,6 +3802,10 @@ class TaskDefinition(object):
                 raise TaskConfigurationException(
                     "The task is rejected due to incompatible parameters: useRealNumEvents, 'Events per Input file'"
                 )
+            if is_pile_task and (not use_real_nevents) and (not ('number_of_events_per_input_file' in list(task_proto_dict.keys()))):
+                raise TaskConfigurationException(
+                    "The task is rejected - pile tasks required  Events per Input file or useRealNumEvents to be set"
+                )
             self._define_merge_params(step, task_proto_dict, train_production)
             self._check_site_container(task_proto_dict)
 

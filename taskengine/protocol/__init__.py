550765900b61111d256e45df95227804be13edf5
mishaborodin <borodin.misha@gmail.com>
2022-02-16 18:05:55 +0100

    Add AMI error to repeated

--- taskengine/protocol/__init__.py
+++ taskengine/protocol/__init__.py
@@ -540,6 +540,7 @@ class TaskDefConstants(object, metaclass=Constants):
     DEFAULT_KILL_JOB_CODE = Constant(9)
 
     ERRORS_TO_REPEAT = ['http status code: 503',
-                        'Connection broken: IncompleteRead']
+                        'Connection broken: IncompleteRead',
+                        '[Errno 111] Connection refused']
     TRANSIENT_ERROR_ATTEMPTS = 1
     REPEAT_ATTEMPT_MESSAGE = 'Repeated because of a transient error'

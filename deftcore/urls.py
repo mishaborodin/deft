5ef9b09ea724d1f2d5db7e395e1c97043f3d9c1a
mishaborodin <borodin.misha@gmail.com>
2024-04-08 18:45:23 +0200

    Prepare for migration

--- deftcore/urls.py
+++ deftcore/urls.py
@@ -1,6 +1,6 @@
 __author__ = 'Dmitry Golubkov'
 
-from django.conf.urls import include, url
+# from django.conf.urls import include, url
 from tastypie.api import Api
 from api import views
 from api.resources import InstanceResource, RequestResource, TaskResource, TRequestResource, TStepResource
@@ -18,9 +18,9 @@ default_api.register(TRequestResource())
 default_api.register(TStepResource())
 
 urlpatterns = [
-    url(r'^$', lambda r: HttpResponseRedirect(
-        reverse('api_dispatch_list', kwargs={'resource_name': 'instance', 'api_name': 'v1'}))),
-    url(r'^test/', views.test_view),
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(default_api.urls)),
+    # url(r'^$', lambda r: HttpResponseRedirect(
+    #     reverse('api_dispatch_list', kwargs={'resource_name': 'instance', 'api_name': 'v1'}))),
+    # url(r'^test/', views.test_view),
+    # url(r'^admin/', admin.site.urls),
+    # url(r'^api/', include(default_api.urls)),
 ]
